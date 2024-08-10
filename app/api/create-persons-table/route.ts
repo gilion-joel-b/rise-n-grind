44e2f04195e842a6a72201a6ffc97fa786a258fb
Joel W. Blomster <joel.blomster@gilion.com>
2024-08-10 09:56:38 +0200

    added vercel postgres

---
+++ app/api/create-persons-table/route.ts
@@ -0,0 +1,16 @@
+import { sql } from '@vercel/postgres';
+import { NextResponse } from 'next/server';
+ 
+export async function GET(request: Request) {
+  try {
+    const result =
+      await sql`CREATE TABLE IF NOT EXISTS Persons(
+          ID int NOT NULL AUTO_INCREMENT,
+          Name varchar(255),
+        );
+        `;
+    return NextResponse.json({ result }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ error }, { status: 500 });
+  }
+}
