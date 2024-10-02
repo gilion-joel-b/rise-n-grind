ef06755b85bea1f425fca41be5a8958dd21f52f6
Joel W. Blomster <joel.blomster@gilion.com>
2024-10-02 16:00:44 +0200

    added analytics and speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Providers from "./providers";
+import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,6 +22,8 @@ export default function RootLayout({
     <html lang="en">
       <Providers>
         <body className={inter.className}>{children}</body>
+        <SpeedInsights />
+        <Analytics />
       </Providers>
     </html>
   );
