826c6ec9fbbd9c64a2e14714d65179a97ca279b4
Joel W. Blomster <joel.blomster@gilion.com>
2025-01-06 15:42:08 +0100

    It's no longer new years, kill the celebration

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -7,8 +7,6 @@ import { MonthlyChampion } from "@/components/blocks/monthly-champion"
 import { Profile } from "@/components/blocks/profile"
 import { Incrementer } from "@/components/blocks/incrementer"
 import { BarChartPinnar } from "@/components/blocks/bar-chart-pinnar"
-import { Fireworks } from '@fireworks-js/react'
-
 
 export default function Home() {
   const router = useRouter()
@@ -23,18 +21,6 @@ export default function Home() {
 
   return (
     <main className="flex text-white min-h-screen flex-col items-center justify-center gap-8 p-2">
-      <Fireworks
-        options={{ opacity: 0.5 }}
-        style={{
-          top: 0,
-          left: 0,
-          width: '100%',
-          height: '100%',
-          position: 'fixed',
-          background: '#000',
-          zIndex: -1
-        }}
-      />
       <Profile />
       <h1 className="text-4xl font-bold">This months Pinnar</h1>
       <MonthlyChampion persons={persons} />
