ba4a3060d3a527fc990fcaf977b32864712860a2
Joel W. Blomster <joel.blomster@gilion.com>
2025-02-10 07:23:18 +0100

    Redirect to new rise and grind

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 import { useGetPersonsQuery } from "../queries"
 import { getCookie } from "cookies-next"
-import { useRouter } from "next/navigation"
+import { permanentRedirect, useRouter } from "next/navigation"
 import { useProfileContext } from "../../components/context/context"
 import { MonthlyChampion } from "@/components/blocks/monthly-champion"
 import { Profile } from "@/components/blocks/profile"
@@ -9,6 +9,8 @@ import { Incrementer } from "@/components/blocks/incrementer"
 import { BarChartPinnar } from "@/components/blocks/bar-chart-pinnar"
 
 export default function Home() {
+  return permanentRedirect("https://rise-and-grind.replit.app/login")
+
   const router = useRouter()
   const { setPerson } = useProfileContext()
   const { persons } = useGetPersonsQuery()
@@ -19,13 +21,14 @@ export default function Home() {
 
   if (!persons || !setPerson) return null
 
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-center gap-8 p-2">
-      <Profile />
+      {/*<Profile />
       <h1 className="text-4xl font-bold">This months Pinnar</h1>
       <MonthlyChampion persons={persons} />
       <BarChartPinnar />
-      <Incrementer />
+      <Incrementer />*/}
     </main >
   )
 }
